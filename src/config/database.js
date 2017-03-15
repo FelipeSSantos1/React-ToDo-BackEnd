16bb7129a16f1ed27e995265b1ffa3628f4cad15
felipemillhouse <felipemillhouse@gmail.com>
2017-03-15 15:07:14 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -2,4 +2,4 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
 //module.exports = mongoose.connect('mongodb://mdbtodo:27017')
-module.exports = mongoose.connect('mongodb://felipe:12345@mdbtodo:27017/tododb');
+module.exports = mongoose.connect('mongodb://userKH7:g8lr6gbKgg4iXojD@todomongodb:27017/todomdb');
